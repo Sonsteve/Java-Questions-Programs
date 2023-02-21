b77d066b75b895ae8f37fa08e31be0264467c61f
Dixson steven <121145215+Sonsteve@users.noreply.github.com>
2023-02-22 00:06:12 +0530

    Factorial

--- Q02_Factorial.java
+++ Q02_Factorial.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Factorial{
-    public static void main(String args[]){
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the value: ");
         int val = in.nextInt();
